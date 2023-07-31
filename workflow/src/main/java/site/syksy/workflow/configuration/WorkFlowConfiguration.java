50de8cc2ab726fc1582846326b14d96ce2ac4d97
ifreeky <ifreeky@163.com>
2023-07-31 23:28:05 +0800

    增加自定义数据源与事务管理器

--- workflow/src/main/java/site/syksy/workflow/configuration/WorkFlowConfiguration.java
+++ workflow/src/main/java/site/syksy/workflow/configuration/WorkFlowConfiguration.java
@@ -1,5 +1,6 @@
 package site.syksy.workflow.configuration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionManager;
 import site.syksy.workflow.properties.H2Properties;
+import site.syksy.workflow.transaction.CustomTransactionManger;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.Objects;
 
 /**
  * @author ifreeky
@@ -22,10 +25,17 @@ public class WorkFlowConfiguration {
 
     @Resource
     private H2Properties h2Properties;
+    @Resource
+    private DataSource dataSource;
+    @Resource
+    private CustomTransactionManger customTransactionManger;
 
     @Bean
     @ConditionalOnMissingBean
     public DataSource dataSource() {
+        if (Objects.nonNull(dataSource)) {
+            return dataSource;
+        }
         return DataSourceBuilder.create().driverClassName(h2Properties.getDriverClassName())
                 .url(h2Properties.getUrl())
                 .username(h2Properties.getUsername())
@@ -34,8 +44,12 @@ public class WorkFlowConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public TransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
+    @ConditionalOnBean(DataSource.class)
+    public TransactionManager transactionManager(DataSource dataSource) {
+        if (Objects.nonNull(customTransactionManger)) {
+            return customTransactionManger.getTransactionManager();
+        }
+        return new DataSourceTransactionManager(dataSource);
     }
 
 
